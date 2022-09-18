254ab917603f208752ca6b2d4740cbfa7549af30
joedoe <doelfer@googlemail.com>
2022-09-18 20:40:19 +0200

    finishing chapter three

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -115,12 +115,30 @@ object List: // `List` companion object. Contains functions for creating and wor
   def filter[A](as: List[A], f: A => Boolean): List[A] =
     foldRight(as, Nil: List[A], (h,t) => if f(h) then Cons(h,t) else t)    
 
-  def flatMap[A,B](as: List[A], f: A => List[B]): List[B] = ???
+  def flatMap[A,B](l: List[A], f: A => List[B]): List[B] =
+    concat(map(l, f))
 
-  def filterViaFlatMap[A](as: List[A], f: A => Boolean): List[A] = ???
+  def filterViaFlatMap[A](l: List[A], f: A => Boolean): List[A] =
+    flatMap(l, a => if f(a) then List(a) else Nil)
 
-  def addPairwise(a: List[Int], b: List[Int]): List[Int] = ???
+  def addPairwise(a: List[Int], b: List[Int]): List[Int] = (a,b) match
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1,t1), Cons(h2,t2)) => Cons(h1+h2, addPairwise(t1,t2))
 
-  // def zipWith - TODO determine signature
+  def zipWith[A, B,C](a: List[A], b: List[B], f: (A, B) => C): List[C] = (a,b) match
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2, f))
 
-  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = ???
+  @annotation.tailrec
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match
+    case Nil => sub == Nil
+    case _ if startsWith(sup, sub) => true
+    case Cons(h,t) => hasSubsequence(t, sub)
+
+  @annotation.tailrec
+  def startsWith[A](l: List[A], prefix: List[A]): Boolean = (l,prefix) match
+    case (_,Nil) => true
+    case (Cons(h,t),Cons(h2,t2)) if h == h2 => startsWith(t, t2)
+    case _ => false    
